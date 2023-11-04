732740f245a2299e7ac455312b81c00758962da1
Khabi <khabimfw@gmail.com>
2023-11-04 14:21:31 +0300

    Docker

--- src/main/kotlin/hammersystemspizza/plugins/Serialization.kt
+++ src/main/kotlin/hammersystemspizza/plugins/Serialization.kt
@@ -15,12 +15,12 @@ import kotlinx.serialization.Serializable
 data class Pizza(val name: String, val description: String, val price: Int, val img_src: String)
 
 val pizzas = listOf(
-    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://drive.google.com/file/d/1n-XhtaIU0EJykausdNLCZ0zSgir9odSt/view?usp=sharing"),
-    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://drive.google.com/file/d/1En2BccGQ-3BSEtB8WE2NFeD9RrgY3zxi/view?usp=sharing"),
-    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://drive.google.com/file/d/1ciSI8tkOVozZtvF5rTeXWl0sSITMNA4b/view?usp=sharing"),
-    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://drive.google.com/file/d/1n-XhtaIU0EJykausdNLCZ0zSgir9odSt/view?usp=sharing"),
-    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://drive.google.com/file/d/1En2BccGQ-3BSEtB8WE2NFeD9RrgY3zxi/view?usp=sharing"),
-    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://drive.google.com/file/d/1ciSI8tkOVozZtvF5rTeXWl0sSITMNA4b/view?usp=sharing"),
+    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://github.com/KhabiKd/imagesRepo/blob/main/pepper_and_sausage.png"),
+    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://github.com/KhabiKd/imagesRepo/blob/main/mix.png"),
+    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://github.com/KhabiKd/imagesRepo/blob/main/mashroom_and_tomato.png"),
+    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://github.com/KhabiKd/imagesRepo/blob/main/pepper_and_sausage.png"),
+    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://github.com/KhabiKd/imagesRepo/blob/main/mix.png"),
+    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://github.com/KhabiKd/imagesRepo/blob/main/mashroom_and_tomato.png"),
 )
 
 fun Application.configureSerialization() {
