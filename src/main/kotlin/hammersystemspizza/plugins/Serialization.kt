62fb17c2ba8fcbe32bd764495777210fbc69ec88
Khabi <khabimfw@gmail.com>
2023-11-04 16:39:13 +0300

    Docker

--- src/main/kotlin/hammersystemspizza/plugins/Serialization.kt
+++ src/main/kotlin/hammersystemspizza/plugins/Serialization.kt
@@ -15,12 +15,12 @@ import kotlinx.serialization.Serializable
 data class Pizza(val name: String, val description: String, val price: Int, val img_src: String)
 
 val pizzas = listOf(
-    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://github.com/KhabiKd/imagesRepo/blob/main/pepper_and_sausage.png"),
-    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://github.com/KhabiKd/imagesRepo/blob/main/mix.png"),
-    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://github.com/KhabiKd/imagesRepo/blob/main/mashroom_and_tomato.png"),
-    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://github.com/KhabiKd/imagesRepo/blob/main/pepper_and_sausage.png"),
-    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://github.com/KhabiKd/imagesRepo/blob/main/mix.png"),
-    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://github.com/KhabiKd/imagesRepo/blob/main/mashroom_and_tomato.png"),
+    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://firebasestorage.googleapis.com/v0/b/pizza-99486.appspot.com/o/pepper_and_sausage.png?alt=media&token=567106f3-7a70-47e1-9360-c8dbe8b92b13"),
+    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://firebasestorage.googleapis.com/v0/b/pizza-99486.appspot.com/o/mix.png?alt=media&token=836b75e6-e8e4-45f1-b4fa-45fd56e88c79"),
+    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://firebasestorage.googleapis.com/v0/b/pizza-99486.appspot.com/o/mashroom_and_tomato.png?alt=media&token=fa354961-0dc9-4e90-a8bb-d24e9090396f"),
+    Pizza("Аррива", "Колбаски, перец чили, оригано, острый соус", 400, "https://firebasestorage.googleapis.com/v0/b/pizza-99486.appspot.com/o/pepper_and_sausage.png?alt=media&token=567106f3-7a70-47e1-9360-c8dbe8b92b13"),
+    Pizza("Микс", "Салями, желтый перец, помидоры, увеличнное количество сыра", 350, "https://firebasestorage.googleapis.com/v0/b/pizza-99486.appspot.com/o/mix.png?alt=media&token=836b75e6-e8e4-45f1-b4fa-45fd56e88c79"),
+    Pizza("Грибы и помидоры", "Шампиньоны, моцарелла, помидоры в увеличенном количестве", 600, "https://firebasestorage.googleapis.com/v0/b/pizza-99486.appspot.com/o/mashroom_and_tomato.png?alt=media&token=fa354961-0dc9-4e90-a8bb-d24e9090396f"),
 )
 
 fun Application.configureSerialization() {
